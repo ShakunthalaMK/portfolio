7494360c117e8c3e62c3fad21f0da91076a46012
Raj <raja.sahoo@costrategix.com>
2016-07-26 00:09:17 +0530

    Lazy load & compressed Image

--- contact.php
+++ contact.php
@@ -1,42 +1,29 @@
-<?php
+<?php 
+// Send  email 
+require_once(__DIR__.'/lib/PHPMailer/class.phpmailer.php');
+require_once(__DIR__.'/lib/PHPMailer/class.smtp.php');
 
-// configure
-$from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'Demo contact form <shakunthalamk19@gmail.com>';
-$subject = 'New message from contact form';
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-
-// let's do the sending
-
-try
-{
-    $emailText = "You have new message from contact form\n=============================\n";
-
-    foreach ($_POST as $key => $value) {
-
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-        }
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPDebug = 0;
+    $mail->Debugoutput = 'html';
+    $mail->IsHTML(true);
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth = true;
+    $mail->Username = "cosselenium@gmail.com";
+    $mail->Password = "wordplz1";
+    $mail->setFrom('cosselenium@gmail.com');//From which Email address.
+//Set who the message is to be sent to
+    $mail->AddAddress('r.sattamsahoo@costrategix.com', 'Raj');
+//Set the subject line
+    $mail->Subject = Some Tried to Contact-Myrollercoster.com,;
+    $mail->Body = "Name:- $_GET['name']<br>Email:- $_GET['email']<br>Phone:- $_GET['phone']<br>Message to You:- $_GET['message']" ; //Set body message.
+    $mail->AddAttachment(__DIR__."/$filename");//Add attachment.
+    if (!$mail->send()) {
+        $mail="Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        $mail="Message sent!";
     }
-
-    mail($sendTo, $subject, $emailText, "From: " . $from);
-
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-}
-catch (\Exception $e)
-{
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
-
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
-    
-    header('Content-Type: application/json');
-    
-    echo $encoded;
-}
-else {
-    echo $responseArray['message'];
-}
+     ?>
